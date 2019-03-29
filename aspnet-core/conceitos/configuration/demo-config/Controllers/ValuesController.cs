2da9003675784d01b4a261e318a816fd74d77c3b
Felipe Silva Martins Salvador <fsalvador@lnxcit021959.cit>
2019-03-29 14:47:15 -0300

    projeto de aplicação

---
+++ aspnet-core/conceitos/configuration/demo-config/Controllers/ValuesController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace demo_config.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly ConnectionString option;
+        public ValuesController(ConnectionString option)
+        {
+            this.option = option;
+
+        }
+        // GET api/values
+        [HttpGet]
+        public ActionResult<ConnectionString> Get()
+        {
+            return option;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+
+    public class ConnectionString
+    {
+        public string DefaultConnection { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
